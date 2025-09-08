17ae37d060a14b3c14c4d9ec1a6e943fb3ee0cbe
Symphony <mnurfauzanakmal160108@gmail.com>
2025-09-08 11:32:42 +0800

    add initial PHP files for database, header, footer, sidebar, and login processing; create index.php with navbar, sidebar, and image slider

--- admin-site/index.php
+++ admin-site/index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Website Rolis</title>
+    <title>Website Image Slider</title>
     
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="../assets/css/style.css" />
     <!-- JS -->
     <script src="assets/js/script.js" defer></script>
     <!-- FontAwesome (wajib untuk icon) -->
@@ -45,7 +45,7 @@
   <section class="main swiper mySwiper" id="home">
     <div class="wrapper swiper-wrapper">
       <div class="slide swiper-slide">
-        <img src="assets/img/bg1A.jpg" alt="" class="image" />
+        <img src="assets/images/bg1A.jpg" alt="" class="image" />
         <div class="image-data">
           <span class="text">Hello, Welcome To My Portofolio</span>
         </div>
