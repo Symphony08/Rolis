8a61f827874dda7c0bced0df623c5a9a52d758b7
Symphony <mnurfauzanakmal160108@gmail.com>
2025-09-08 16:08:39 +0800

    Update sidebar links to English and enhance logout button styling

--- admin-site/includes/sidebar.php
+++ admin-site/includes/sidebar.php
@@ -2,11 +2,11 @@
 <aside class="sidebar">
   <ul>
     <li><a href="index_admin.php">Home</a></li>
-    <li><a href="#">Produk</a></li>
+    <li><a href="#">Our Product</a></li>
     <li><a href="#">Customers</a></li>
     <li><a href="#">Service</a></li>
     <li><a href="#">Transaksi</a></li>
-    <li><a href="#">Hubungi Kami</a></li>
+    <li><a href="#">Contact</a></li>
     <li><a href="logout.php" class="btn-logout">Logout</a></li>
   </ul>
 </aside>
