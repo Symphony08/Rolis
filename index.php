32bbf166470ee1f778a3367ad025b3a8fbd464a1
Symphony <mnurfauzanakmal160108@gmail.com>
2025-09-08 14:20:32 +0800

    Update admin dashboard layout and styles; implement sidebar and dropdown functionality

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <div class="login-container">
     <!-- Header -->
     <div class="login-header">
-      <img src="assets/img/logo.jpg" alt="Logo">
+      <img src="assets/img/logo.png" alt="Logo">
       <h2>Selamat Datang Admin</h2>
     </div>
 
