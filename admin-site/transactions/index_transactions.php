f4a3b557d7916cf56d31db5cc04243ba6dab4b62
Symphony <mnurfauzanakmal160108@gmail.com>
2025-10-20 11:00:36 +0800

    Add 'tipe' column to transaction display and update query to include product type

--- admin-site/transactions/index_transactions.php
+++ admin-site/transactions/index_transactions.php
@@ -49,6 +49,7 @@ $rows = $transactionController->show()->fetch_all(MYSQLI_ASSOC);
             <th class="text-center">Pelanggan</th>
             <th class="text-center">Produk</th>
             <th class="text-center">Warna</th>
+            <th class="text-center">Tipe</th>
             <th class="text-center">Nomor Mesin</th>
             <th class="text-center">Nomor Body</th>
             <th class="text-center">Tanggal Berakhir Garansi</th>
@@ -66,6 +67,7 @@ $rows = $transactionController->show()->fetch_all(MYSQLI_ASSOC);
                 <td class="text-center"><?= htmlspecialchars($row['nama_pelanggan']) ?></td>
                 <td class="text-center"><?= htmlspecialchars($row['nama_produk']) ?></td>
                 <td class="text-center"><?= htmlspecialchars($row['warna']) ?></td>
+                <td class="text-center"><?= htmlspecialchars($row['tipe']) ?></td>
                 <td class="text-center"><?= htmlspecialchars($row['nomor_mesin']) ?></td>
                 <td class="text-center"><?= htmlspecialchars($row['nomor_body']) ?></td>
                 <td class="text-center"><?= date("d-m-Y", strtotime($row['tanggal_garansi'])) ?></td>
